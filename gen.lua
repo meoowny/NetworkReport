01a19dc5daed83dcec0245215fe93ae9ed1a7ea2
Mony <mony_hill@qq.com>
2024-03-27 16:13:33 +0800

    new(10, 11): 第五周实验开始；feat(gen.lua): 功能完善

--- gen.lua
+++ gen.lua
@@ -1,8 +1,12 @@
 -- https://www.cnblogs.com/xdao/p/lua_file.html
--- UTF-8 的编码问题：https://zhuanlan.zhihu.com/p/157815053
+-- UTF-8 的编码问题: https://zhuanlan.zhihu.com/p/157815053
+-- TypedLua: https://github.com/teal-language/tl https://zhuanlan.zhihu.com/p/40300705
 local json = require("json")
 local args = {...}
 local version = "0.0.1"
+
+local info_template_path = "./template/info.json"
+local report_template_path = "./template/single.typ"
 
 -- 单个实验报告的引入函数
 local single = function(file, file_info, date)
@@ -13,7 +17,33 @@ local single = function(file, file_info, date)
     file:write("#include \"../" .. file_info["id"] .. "/" .. file_info["id"] .. ".typ\"\n\n")
 end
 
-local chackhealth = function(info)
+local existFile = function (filename, mode)
+    if mode == nil then mode = "r" end
+    local file = io.open(filename, mode)
+    if file == nil then
+        return false
+    else
+        file:close()
+        return true
+    end
+end
+
+local copyFile = function(source, destination)
+    -- 读取源文件内容
+    local src, error_info = io.open(source, "r")
+    assert(src ~= nil, error_info)
+    local content = src:read("a")
+    src:close()
+    -- 写入目标文件
+    local dst, error_info_ = io.open(destination, "a")
+    assert(dst ~= nil, error_info_)
+    dst:write(content)
+    dst:close()
+    return true
+end
+
+-- 检查 json 文件完整性
+local checkhealth = function(info)
     if info["date"] == nil or info["files"] == nil then
         return false
     else
@@ -28,34 +58,22 @@ local chackhealth = function(info)
     return true
 end
 
-local copyFile = function(source, destination)
-    return true
-end
-
 if args[1] == "c" then
     -- 编译
-    if args[2] == nil then
-        print("Json filename missed.")
-        os.exit()
-    end
+    assert(args[2] ~= nil, "Json filename missed.")
     local target_name = args[2]
 
     -- 获取实验报告信息
-    io.input("info/" .. target_name .. ".json")
-    local info_text = io.read("a")
+    local target_file = io.open("info/" .. target_name .. ".json", "r")
+    assert(target_file ~= nil, "Target file missed.")
+    local info_text = target_file:read("a")
     local info = json.decode(info_text)
-    io.close()
-    if not chackhealth(info) then
-        print("Broken info file.")
-        os.exit()
-    end
+    target_file:close()
+    assert(checkhealth(info), "Broken info file.")
 
     -- 打开目标报告
     local file = io.open("./dist/" .. target_name .. ".typ", "w")
-    if file == nil then
-        print("Cannot open target file.")
-        os.exit()
-    end
+    assert(file ~= nil, "Cannot open target file.")
 
     -- 生成目标报告
     local outFile = "2254198_段子涛"
@@ -72,19 +90,36 @@ if args[1] == "c" then
 
     -- 目标报告生成结束，调用系统指令进行编译
     os.execute("typst compile --font-path ./fonts/ --root ./ ./dist/" .. target_name .. ".typ")
-    print("Complete")
+    print("Compile completed")
 elseif args[1] == "g" then
     -- 生成 json 或单报告文件
-    if args[2] == nil then
-        print("Mode missed.")
-        os.exit()
-    elseif args[3] == nil then
-        print("Info filename missed.")
-        os.exit()
-    elseif args[2] == 'j' then
-        -- 需要检查模版 json 是否还在
+    assert(args[2] ~= nil, "Mode missed.")
+    assert(args[3] ~= nil, "Info filename missed.")
+    local target_path = "info/" .. args[3] .. ".json"
+    if args[2] == 'j' then
+        -- 检查模版 json 是否还在
+        assert(existFile(info_template_path), "Info template file missed.")
+        copyFile(info_template_path, target_path)
     elseif args[2] == 'r' then
+        assert(existFile(report_template_path), "Report template file missed.")
+        local target_file = io.open(target_path, "r")
+        assert(target_file ~= nil, "Target file missed.")
+
+        local info_text = target_file:read("a")
+        local info = json.decode(info_text)
+        target_file:close()
+        assert(checkhealth(info), "Broken info file.")
+
+        for _, v in ipairs(info["files"]) do
+            local temp_name = "./" .. v["id"] .. "/" .. v["id"] .. ".typ"
+            if not existFile(temp_name) then
+                os.execute("mkdir " .. v["id"])
+                copyFile(report_template_path, temp_name)
+            end
+        end
     end
+
+    print("Generate completed.")
 elseif args[1] == "h" then
     -- help
     print("c <meta file(json)> -> compile")
